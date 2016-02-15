f3ce2db8f4054f4dd6a82a9b910ad13528340e2d
Denis Vasiliev <vasilievminsk@gmail.com>
2016-02-15 11:17:41 +0300

    Adding a front-end part of application

---
+++ public/js/chirpApp.js
@@ -0,0 +1,47 @@
+var app = angular.module('chirpApp', ['ngRoute']);
+
+app.config(function($routeProvider){
+  $routeProvider
+    //the timeline display
+    .when('/', {
+      templateUrl: 'main.html',
+      controller: 'mainController'
+    })
+    //the login display
+    .when('/login', {
+      templateUrl: 'login.html',
+      controller: 'authController'
+    })
+    //the signup display
+    .when('/register', {
+      templateUrl: 'register.html',
+      controller: 'authController'
+    });
+});
+
+app.controller('mainController', ['$scope', function($scope){
+	$scope.posts = [];
+	$scope.newPost = {created_by: '', text: '', create_at: ''};
+
+	$scope.post = function(){
+    $scope.newPost.created_at = Date.now();
+    $scope.posts.push($scope.newPost);
+    $scope.newPost = {created_by: '', text: '', created_at: ''};
+  };
+}]);
+
+app.controller('authController', ['$scope', function($scope){
+  $scope.user = {username: '', password: ''};
+  $scope.error_message = '';
+
+  $scope.login = function(){
+    //placeholder until authentication is implemented
+    $scope.error_message = 'login request for ' + $scope.user.username;
+  };
+
+  $scope.register = function(){
+    //placeholder until authentication is implemented
+    $scope.error_message = 'registeration request for ' + $scope.user.username;
+  };
+}]);
+
